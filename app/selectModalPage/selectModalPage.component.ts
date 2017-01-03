ed3e5762b85e24d450b9581aabb64ca6a69f0fc9
Boykov Dmitriy <boikovdmitriy@mail.ru>
2017-01-03 22:57:23 +0300

    removed unused links

--- app/selectModalPage/selectModalPage.component.ts
+++ app/selectModalPage/selectModalPage.component.ts
@@ -7,9 +7,7 @@ import { Component } from "@angular/core";
 			template: `
 <ul>
 	<li><a routerLink="/simple-modal">Простое модальное окно реализованное стандартными средствами Angular</a></li>
-	<li><a routerLink="/js-modal">Модальные окнa с ручным редактированием dom</a></li>
 	<li><a routerLink="/dynamic-modal">Модальные окна с динамическим рендерингом средствами Angular</a></li>
-	<li><a routerLink="/static-modal">Модальные окна обычный компонентный подход</a></li>
 </ul>
 `
            })
